f01189cf1c1c73d4b0ac2b5d912b8a0ecf08cb90
Marius-Cornel Ilasca <ilasca_marius@yahoo.com>
2026-02-13 11:57:03 +0200

    Update carousel.js

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,19 +1,22 @@
 let counter = 0;
-let slide = document.querySelector('.carousel-slide');
-let images = document.querySelectorAll('.carousel-slide img');
 
-document.querySelector('.next').addEventListener('click', nextSlide);
-document.querySelector('.prev').addEventListener('click', prevSlide);
+document.addEventListener("DOMContentLoaded", function () {
+  const slide = document.querySelector('.carousel-slide');
+  const images = document.querySelectorAll('.carousel-slide img');
 
-function nextSlide() {
-  counter = (counter + 1) % images.length;
-  slide.style.transform = `translateX(${-counter * 100}%)`;
-}
+  document.querySelector('.next').addEventListener('click', nextSlide);
+  document.querySelector('.prev').addEventListener('click', prevSlide);
 
-function prevSlide() {
-  counter = (counter - 1 + images.length) % images.length;
-  slide.style.transform = `translateX(${-counter * 100}%)`;
-}
+  function nextSlide() {
+    counter = (counter + 1) % images.length;
+    slide.style.transform = `translateX(${-counter * 100}%)`;
+  }
 
-// Auto-play
-setInterval(nextSlide, 5000);
+  function prevSlide() {
+    counter = (counter - 1 + images.length) % images.length;
+    slide.style.transform = `translateX(${-counter * 100}%)`;
+  }
+
+  // Auto-play
+  setInterval(nextSlide, 5000);
+});
